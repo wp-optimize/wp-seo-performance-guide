69cccd648674cdb36ca0ec02c522d0f330725b23
Masih Jahangiri <masihjahan@gmail.com>
2025-03-04 17:01:53 +0330

    update url in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://WPOptimize.github.io',
+  url: 'https://wp-optimize.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'WPOptimize.github.io', // Usually your GitHub org/user name.
-  projectName: 'WPOptimize', // Usually your repo name.
+  organizationName: 'wp-optimize.github.io', // Usually your GitHub org/user name.
+  projectName: 'wp-optimize', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
