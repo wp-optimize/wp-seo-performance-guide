1fd89713735cc0fa5bee4f6f45da5b0a067bde65
Masih Jahangiri <masihjahan@gmail.com>
2025-03-05 13:57:09 +0330

    update title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,9 +45,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'WordPress Performance Mastery',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'WordPress Performance Mastery Logo',
         src: 'img/logo.svg',
       },
       items: [
